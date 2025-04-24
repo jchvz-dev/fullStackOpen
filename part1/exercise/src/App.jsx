4b5bf0378690f21b1a1cf6d9816134214e3af040
jchvz <jchvz.dev@gmail.com>
2025-04-24 11:05:35 -0700

    Completed excercise 1.1

---
+++ part1/exercise/src/App.jsx
@@ -0,0 +1,35 @@
+const Header = (props) => (
+    <h1>{props.course}</h1>
+)
+
+const Content = (props) => (
+    <>
+        <p>{props.parts[0]} {props.exercises[0]}</p>
+        <p>{props.parts[1]} {props.exercises[1]}</p>
+        <p>{props.parts[2]} {props.exercises[2]}</p>
+    </>
+)
+
+const Total = (props) => (
+    <p>Number of exercises {props.num1 + props.num2 + props.num3}</p>
+)
+
+const App = () => {
+    const course = 'Half Stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
+  
+    return (
+      <div>
+        <Header course={course} />
+        <Content parts={[part1,part2,part3]} exercises={[exercises1,exercises2,exercises3]} />
+        <Total num1={exercises1} num2={exercises2} num3={exercises3} />
+      </div>
+    )
+  }
+
+export default App
