b3ef26552b6fb2f3a323bb7acc8cac7645fc4ccd
jchvz <jchvz.dev@gmail.com>
2025-05-16 12:31:54 -0700

    Completed exercise 1.5

--- part1/exercise/src/App.jsx
+++ part1/exercise/src/App.jsx
@@ -11,36 +11,34 @@ const Total = (props) => (
 )
 
 const Content = (props) => (
-    <>
-    <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
-    <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
-    <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
-    </>
+    props.parts.map(part => <Part name={part.name} exercises={part.exercises} />)
 )
 
 const App = () => {
 
-    const course = 'Half Stack application development'
-    const parts = [
-        {
-        name: 'Fundamentals of React',
-        exercises: 10
-        },
-        {
-        name: 'Using props to pass data',
-        exercises: 7
-        },
-        {
-        name: 'State of a component',
-        exercises: 14
-        }
-    ]
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+      }
   
     return (
         <div>
-            <Header course={course} />
-            <Content parts={parts} />
-            <Total parts={parts} />
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
         </div>
     )
 }
