30bc41096884df1a060c5c6af39c975218ad245f
jchvz <jchvz.dev@gmail.com>
2025-05-19 12:16:56 -0700

    Completed exercise 1.6

---
+++ part1/1.6/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+const App = () => {
+    // save clicks of each button to its own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    const addGood = () => {
+        setGood(good + 1)
+    }
+
+    const addNeutral = () => {
+        setNeutral(neutral + 1)
+    }
+
+    const addBad = () => {
+        setBad(bad + 1)
+    }
+
+    return (
+        <div>
+            <h1>Give feedback</h1>
+            <button onClick={addGood}>Good</button>
+            <button onClick={addNeutral}>Neutral</button>
+            <button onClick={addBad}>Bad</button>
+            <h2>Statistics</h2>
+            <p>Good: {good}</p>
+            <p>Neutral: {neutral}</p>
+            <p>Bad: {bad}</p>
+        </div>
+    )
+}
+
+export default App
