3a62418fb4d8cc8ff3dbf7ed27bb4a17f77cb115
jchvz <jchvz.dev@gmail.com>
2025-05-23 12:42:49 -0700

    Completed exercise 2.9

--- part2/06-10/src/App.jsx
+++ part2/06-10/src/App.jsx
@@ -2,21 +2,30 @@ import { useState } from 'react'
 
 const App = () => {
     const [contacts, setContacts] = useState([
-        { name: 'Arto Hellas', number: '040-1234567' }
-    ]) 
+        { name: 'Arto Hellas', number: '040-123456', id: 1 },
+        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    ])
+    const [filteredContacts, setFilteredContacts] = useState(contacts)
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
+    const [filter, setFilter] = useState('')
 
     const addContact = (event) => {
         event.preventDefault()
-        if (contacts.some(contact => contact.name == newName)) {
+        if (contacts.some(contact => contact.name.toLowerCase() == newName.toLowerCase())) {
             alert(`${newName} is already added to phonebook`)
         } else {
             const contactObject = {
                 name: newName,
-                number: newNumber
+                number: newNumber,
+                id: contacts.length + 1
             }
-            setContacts(contacts.concat(contactObject))
+            const updatedContacts = contacts.concat(contactObject)
+            setContacts(updatedContacts)
+            setFilteredContacts(filter === '' ? updatedContacts : updatedContacts.filter(contact => contact.name.toLowerCase().includes(filter)));
+
         }
         setNewName('')
         setNewNumber('')
@@ -28,10 +37,22 @@ const App = () => {
     const handleNumberChange = (event) => {
         setNewNumber(event.target.value)
     }
+    const handleFilterChange = (event) => {
+        const filterValue = event.target.value
+        setFilter(filterValue)
+        setFilteredContacts(filterValue === '' ? contacts : contacts.filter(contact => contact.name.toLowerCase().includes(filterValue)))
+    }
 
     return (
         <div>
-            <h2>Phonebook</h2>
+            <h1>Phonebook</h1>
+
+            <div>
+                filter shown with: <input value={filter} onChange={handleFilterChange}/>
+            </div>
+
+            <h2>Add a new</h2>
+
             <form onSubmit={addContact}>
                 <div>
                 name: <input value={newName} onChange={handleNameChange}/>
@@ -44,7 +65,7 @@ const App = () => {
                 </div>
             </form>
             <h2>Numbers</h2>
-            {contacts.map((contact) => <p key={contact.name}>{contact.name} {contact.number}</p>)}
+            {filteredContacts.map((contact) => <p key={contact.name}>{contact.name} {contact.number}</p>)}
         </div>
     )
 }
