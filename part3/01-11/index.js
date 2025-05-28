6c580dd38cd7d556440cd57043efa423f01b04e2
jchvz <jchvz.dev@gmail.com>
2025-05-28 11:46:49 -0700

    Completed exercise 3.5

--- part3/01-11/index.js
+++ part3/01-11/index.js
@@ -55,6 +55,23 @@ app.delete('/api/persons/:id', (request, response) => {
   
     response.status(204).end()
 })
+
+const generateId = () => {
+    const randomId = Math.floor(Math.random() * 100)
+    return String(randomId)
+}
+  
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    const person = {
+        'id': generateId(),
+        'name': body.name,
+        'number': body.number,
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
+})
   
 const PORT = 3001
 app.listen(PORT, () => {
