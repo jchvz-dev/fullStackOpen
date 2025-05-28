c61192563d15dc3e85b13d6045cd52b486fecb3f
jchvz <jchvz.dev@gmail.com>
2025-05-28 12:05:12 -0700

    Completed exercise 3.6

--- part3/01-11/index.js
+++ part3/01-11/index.js
@@ -63,6 +63,20 @@ const generateId = () => {
   
 app.post('/api/persons', (request, response) => {
     const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({ 
+            error: 'content missing' 
+        })
+    }
+
+    const nameExists = persons.some(person => person.name === body.name)
+    if (nameExists) {
+        return response.status(400).json({ 
+            error: 'name must be unique' 
+        })
+    }
+
     const person = {
         'id': generateId(),
         'name': body.name,
