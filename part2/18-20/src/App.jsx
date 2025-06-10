031f5e144c81222c9e7e6cfa7e5b7f84eb3c79f5
jchvz <jchvz.dev@gmail.com>
2025-06-10 09:27:32 -0700

    Completed exercise 2.18

---
+++ part2/18-20/src/App.jsx
@@ -0,0 +1,93 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/all'
+
+const FindCountries = ({value, onChangeFilter}) => (
+    <div>
+        Find countries: <input value={value} onChange={onChangeFilter} />
+    </div>
+)
+
+const CountriesList = ({data}) => {
+    if (data.length > 5) {
+        return (
+            <p>Too many matches. Specify another filter.</p>
+        )
+    }
+
+    if (data.length === 0) {
+        return (
+            <p>No matches found. Try another filter.</p>
+        )
+    }
+
+    if (data.length === 1) {
+        return (
+            <CountryData country={data[0]}/>
+        )
+    }
+
+    return (
+        <div>
+            {data.map((country) => <p key={country.name.common}>{country.name.common}</p>)}
+        </div>
+    )
+}
+
+const CountryData = ({country}) => {
+    const Languages = ({list}) => {
+        return (
+            <div>
+                <h3>Languages</h3>
+                <ul>
+                    {list.map((language) => <li key={language}>{language}</li>)}
+                </ul>
+            </div>
+            
+            
+        )
+    }
+    return (
+        <div>
+            <h1>{country.name.common}</h1>
+            <img src={country.flags.png} alt={country.flags.alt} />
+            <p><b>Capital: </b>{country.capital}</p>
+            <p><b>Region: </b>{country.region}</p>
+            <p><b>Area: </b>{country.area}</p>
+            <p><b>Population: </b>{country.population}</p>
+            <Languages list={Object.values(country.languages)} />
+        </div>
+    )
+}
+
+function App() {
+    const [filter, setFilter] = useState('')
+    const [filteredCountries, setFilteredCountries] = useState([])
+    const [countries, setCountries] = useState([])
+
+    useEffect(() => {
+        axios.get(baseUrl)
+        .then(response => {
+            setCountries(response.data)
+            // setFilteredContacts(response.data)
+        })
+    }, [])
+
+    const handleFilterChange = (event) => {
+        const filterValue = event.target.value
+        setFilter(filterValue)
+        setFilteredCountries(filterValue === '' ? countries : countries.filter(country => country.name.common.toLowerCase().includes(filterValue.toLowerCase())))
+    }
+
+    return (
+        <div>
+
+            <FindCountries value={filter} onChangeFilter={handleFilterChange} />
+            <CountriesList data={filteredCountries}/>
+
+        </div>
+    )
+}
+
+export default App
