1ef4b5091ebda85795dd2d5092f526500cf4f00b
jchvz <jchvz.dev@gmail.com>
2025-06-10 11:34:51 -0700

    Completed exercise 2.20

--- part2/18-20/src/App.jsx
+++ part2/18-20/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const api_key = import.meta.env.VITE_SOME_KEY
 const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/all'
 
 const FindCountries = ({value, onChangeFilter}) => (
@@ -9,26 +10,16 @@ const FindCountries = ({value, onChangeFilter}) => (
     </div>
 )
 
-const CountriesList = ({data, onClickShow}) => {
-
+const CountriesList = ({data, onClickShow, weather}) => {
     if (data.length === 1) {
-        return (
-            <CountryData country={data[0]}/>
-        )
+        return <CountryData country={data[0]} weather={weather} />
     }
-
     if (data.length === 0) {
-        return (
-            <p>No matches found. Try another filter.</p>
-        )
+        return <p>No matches found. Try another filter.</p>
     }
-
     if (data.length > 5) {
-        return (
-            <p>Too many matches. Specify another filter.</p>
-        )
+        return <p>Too many matches. Specify another filter.</p>
     }
-
     return (
         <div>
             {data.map((country) => <p key={country.name.common}>{country.name.common} <button onClick={() => onClickShow(country.name.common)}>Show</button></p>)}
@@ -36,7 +27,7 @@ const CountriesList = ({data, onClickShow}) => {
     )
 }
 
-const CountryData = ({country}) => {
+const CountryData = ({country, weather}) => {
     const Languages = ({list}) => {
         return (
             <div>
@@ -45,8 +36,6 @@ const CountryData = ({country}) => {
                     {list.map((language) => <li key={language}>{language}</li>)}
                 </ul>
             </div>
-            
-            
         )
     }
     return (
@@ -58,6 +47,14 @@ const CountryData = ({country}) => {
             <p><b>Area: </b>{country.area}</p>
             <p><b>Population: </b>{country.population}</p>
             <Languages list={Object.values(country.languages)} />
+            <h2>Weather in {country.capital}</h2>
+            {weather &&
+                <>
+                <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt='weather icon' />
+                <p><b>Temperature: </b>{weather.main.temp}° Celsius</p>
+                <p><b>Wind: </b>{weather.wind.speed} m/s</p>
+                </>
+            }
         </div>
     )
 }
@@ -66,14 +63,24 @@ function App() {
     const [filter, setFilter] = useState('')
     const [filteredCountries, setFilteredCountries] = useState([])
     const [countries, setCountries] = useState([])
+    const [weather, setWeather] = useState(null)
 
     useEffect(() => {
         axios.get(baseUrl)
         .then(response => {
             setCountries(response.data)
-            // setFilteredContacts(response.data)
         })
     }, [])
+
+    useEffect(() => {
+        if (filteredCountries.length === 1) {
+            const country = filteredCountries[0]
+            axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&units=metric&appid=${api_key}`)
+            .then(response => {
+                setWeather(response.data)
+            })
+        }
+    }, [filteredCountries])
 
     const handleFilterChange = (event) => {
         const filterValue = event.target.value
@@ -82,16 +89,13 @@ function App() {
     }
 
     const handleOnClickShow = (countryName) => {
-        setFilter(countryName)
         setFilteredCountries(countries.filter(country => country.name.common.includes(countryName)))
     }
 
     return (
         <div>
-
             <FindCountries value={filter} onChangeFilter={handleFilterChange} />
-            <CountriesList data={filteredCountries} onClickShow={handleOnClickShow}/>
-
+            <CountriesList data={filteredCountries} onClickShow={handleOnClickShow} weather={weather}/>
         </div>
     )
 }
